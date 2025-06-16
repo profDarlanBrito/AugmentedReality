e524f9fdbc45281b2b7762ac58cc0a34ff58e1f6
dnune <darlan@ufop.edu.br>
2025-06-16 15:47:19 -0300

    Get fiducial marks and adjust a source image. Program ok

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from PutImagesOnMarks import PutImagesOnMarks
 from getFiducialLocation import getFiducialLocation
 
 if __name__ == "__main__":
-    markImageName = './images/FiducialMarkA4DifferentIDs.png'
+    markImageName = './images/FiducialMarkA4DifferentIDsSmall.png'
     sourceImageName = './images/eu.jpg'
     corners, ids, rejectedImgPoints = getFiducialLocation(markImageName)
     PutImagesOnMarks(ids, corners, sourceImageName, markImageName)
