84d121377ba3c6aae1eb0fd672348507f5de2224
dnune <darlan@ufop.edu.br>
2025-06-18 08:40:55 -0300

    Verification about the operation system

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from PutImagesOnMarks import PutImagesOnMarks
 from getFiducialLocation import getFiducialLocation
+import platform
 
 if __name__ == "__main__":
-    markImageName = './images/FiducialMarkA4DifferentIDsSmall.png'
-    sourceImageName = './images/eu.jpg'
+    if platform.system() == "Windows":
+        print("Running on Windows")
+    elif platform.system() == "Linux":
+        print("Running on Linux")
+    else:
+        print(f"Running on unknown OS: {platform.system()}")
+    markImageName = './Images/FiducialMarkA4DifferentIDsSmall.png'
+    sourceImageName = './Images/eu.jpg'
     corners, ids, rejectedImgPoints = getFiducialLocation(markImageName)
     PutImagesOnMarks(ids, corners, sourceImageName, markImageName)
